0b2510b46634a4dac0b6769d9d57a846ea5b7f17
riccardo-girone <149433192+riccardo-girone@users.noreply.github.com>
2024-01-21 20:00:31 +0100

    feat: aggiunto cursore custom

---
+++ app.js
@@ -0,0 +1,14 @@
+const cursor = document.getElementById("cursor");
+
+  const animateCursor = (e) => {
+    const x = e.clientX,
+          y = e.clientY;
+
+    const keyframes = {
+      transform: `translate(${x}px, ${y}px)`
+    }
+
+    cursor.animate([keyframes], { duration: 0, fill: "forwards" });
+  }
+
+  document.addEventListener("mousemove", animateCursor);
